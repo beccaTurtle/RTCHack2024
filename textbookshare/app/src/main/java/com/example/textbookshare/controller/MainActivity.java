4100697d076d1059b6aab879ad2acb4fd468eb7a
Becca Bogstad <rbogstad@vassar.edu>
2024-02-02 17:34:47 -0500

    try again

---
+++ textbookshare/app/src/main/java/com/example/textbookshare/controller/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.textbookshare.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.textbookshare.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
