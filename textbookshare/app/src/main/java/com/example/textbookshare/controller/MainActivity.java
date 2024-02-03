204ca18d1a5cc2c39b366bc98aa98554cd4254eb
CaroAK <CarolineAliciaKlein@icloud.com>
2024-02-02 20:06:32 -0500

    Add search to MainView

--- textbookshare/app/src/main/java/com/example/textbookshare/controller/MainActivity.java
+++ textbookshare/app/src/main/java/com/example/textbookshare/controller/MainActivity.java
@@ -5,13 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.textbookshare.R;
+import com.example.textbookshare.model.Library;
+import com.example.textbookshare.model.Textbook;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    Library lib;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_search);
+    }
+
+    @Override
+    public void onPerformSearch(String searchTerm) {
+        List<Textbook> matches = lib.search(searchTerm);
+        this.mainView.displaySearchResults(matches);
     }
 
 
