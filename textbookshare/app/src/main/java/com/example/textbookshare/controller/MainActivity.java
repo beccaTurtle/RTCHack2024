037cb9a085893b7153c026ce61b1e2d69db9234e
Becca Bogstad <rbogstad@vassar.edu>
2024-02-02 20:07:25 -0500

    updates

--- textbookshare/app/src/main/java/com/example/textbookshare/controller/MainActivity.java
+++ textbookshare/app/src/main/java/com/example/textbookshare/controller/MainActivity.java
@@ -5,12 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.textbookshare.R;
+import com.example.textbookshare.model.*;
 
 public class MainActivity extends AppCompatActivity {
+
+    Library library;
+    User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_search);
+        setContentView(R.layout.main);
+
+        this.library = new Library();
     }
 }
--- textbookshare/app/src/main/java/com/example/textbookshare/model/Library.java
+++ textbookshare/app/src/main/java/com/example/textbookshare/model/Library.java
@@ -22,6 +22,8 @@ public class Library {
             }
         }
 
+        return textbooks;
+
     }
 
 }
